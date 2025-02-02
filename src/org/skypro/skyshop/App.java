0c074d16a256989b93e3437700a70c7dbe27a2b6
Evgeniy-Zarubin <Uncontrollable69@gmail.com>
2025-02-02 13:59:05 +1000

    Check_My_Work_Basket

--- src/org/skypro/skyshop/App.java
+++ src/org/skypro/skyshop/App.java
@@ -1,8 +1,47 @@
 package org.skypro.skyshop;
 
+import org.skypro.skyshop.basket.ProductBasket;
+import org.skypro.skyshop.product.Product;
+
 public class App {
     public static void main(String[] args) {
+        Product milk = new Product("Молоко", 50.78);
+        Product bread = new Product("Хлеб", 30.20);
+        Product apples = new Product("Яблоки", 100);
+        Product meat = new Product("Мясо", 300.22);
+        Product cheese = new Product("Сыр", 200.3);
+        Product butter = new Product("Масло", 150.83);
 
+        ProductBasket basket = new ProductBasket();
+
+        basket.addProduct(milk);
+        basket.addProduct(bread);
+        basket.addProduct(apples);
+        basket.printAllProducts();
+
+        double totalCost = basket.getTotalCost();
+        System.out.println("Общая стоимость корзины: " + totalCost);
+
+        boolean hasMilk = basket.equalsProductName("Молоко");
+        System.out.println("Есть ли молоко в корзине? " + hasMilk);
+
+        boolean hasMeat = basket.equalsProductName("Мясо");
+        System.out.println("Есть ли мясо в корзине? " + hasMeat);
+
+        basket.clear();
+        basket.printAllProducts();
+
+        double emptyTotalCost = basket.getTotalCost();
+        System.out.println("Стоимость пустой корзины: " + emptyTotalCost);
+
+
+        boolean hasAppleInEmpty = basket.equalsProductName("Яблоки");
+        System.out.println("Есть ли яблоки в пустой корзине? " + hasAppleInEmpty);
+
+        basket.addProduct(meat);
+        basket.addProduct(cheese);
+        basket.addProduct(butter);
+        basket.addProduct(new Product("Сахар", 80));
 
     }
 }
--- src/org/skypro/skyshop/basket/ProductBasket.java
+++ src/org/skypro/skyshop/basket/ProductBasket.java
@@ -1,8 +1,59 @@
 package org.skypro.skyshop.basket;
+import java.util.Arrays;
 
 import org.skypro.skyshop.product.Product;
 
 public class ProductBasket {
-    private Product[] products = new Product[5];
+    private static final int maxProducts = 5;
+    private Product[] products = new Product[maxProducts];
+    private int countProducts = 0;
+
+    public void addProduct(Product product) {
+        if (countProducts >= maxProducts) {
+            System.out.println("Невозможно добавить продукт - корзина полна");
+        } else {
+            products[countProducts++] = product;
+        }
+    }
+
+    public double getTotalCost() {
+        double totalCost = 0;
+        for (int i = 0; i < countProducts; i++) {
+            totalCost += products[i].getPrice();
+        }
+        return totalCost;
+    }
+
+    public void printAllProducts() {
+        if (isEmpty()) {
+            System.out.println("в корзине пусто");
+        } else {
+            for (int i = 0; i < countProducts; i++) {
+                Product product = products[i];
+                System.out.println("Продукт: " + product.getName() + " Цена: " + product.getPrice());
+            }
+            System.out.println("Итого: " + getTotalCost());
+        }
+    }
+
+    public boolean equalsProductName(String name) {
+        for (int i = 0; i < countProducts; ++i) {
+            if (products[i].getName().equals(name)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void clear() {
+        for (int i = 0; i < products.length; i++) {
+            products[i] = null;
+        }
+        countProducts = 0;
+    }
+
+    private boolean isEmpty() {
+        return countProducts == 0;
+    }
 
 }
